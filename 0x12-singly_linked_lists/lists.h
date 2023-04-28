0f77f2393f6cc53c0eaf34d150c780b5b30f108d
DebbieOnGit <deborahgmeme@gmail.com>
2023-04-27 19:37:47 -0700

    task0

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,14 +2,14 @@
 #define LISTS_H
 
 /**
- * struct list_h - singly linked list
+ * struct list_s singly linked list
  * @str: string - (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node
  *
  * Description: singly linked list node structure
  */
-typedef struct list_h
+typedef struct list_s
 {
 	char *str;
 	unsigned int len;
