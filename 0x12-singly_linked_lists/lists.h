ffc43c92633b4f4d732b499419099a19f19c23eb
DebbieOnGit <109739405+DebbieOnGit@users.noreply.github.com>
2023-04-28 05:55:03 +0300

    Update lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,8 +1,10 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stdlib.h>
+
 /**
- * struct list_s singly linked list
+ * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node
@@ -13,11 +15,11 @@ typedef struct list_s
 {
 	char *str;
 	unsigned int len;
-	struct list_h *next;
+	struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
-size_t list_len(const list_);
+size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
